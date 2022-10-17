8aed86bbda02e64fc61b674b03c159b8e8bdefd6
Igor <i.ergakoff@mail.ru>
2022-10-17 20:41:40 +0300

    Решение задания № 34

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/***************************************************************************************
+Задача 34: 
+Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+***************************************************************************************/
+
+int[] array = GetArray(5, 100, 1000);
+Console.WriteLine(String.Join("|", array));
+Console.WriteLine($"В массиве [{String.Join("|", array)}] {EverNum(array)} четных элементов");
+
+int EverNum(int[] Arr)
+{
+    int count = 0;
+    for (int i = 0; i < Arr.Length; i++)
+    {
+        if (Arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+// Метод заполнения массива:
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
